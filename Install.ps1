85d084c20f31f0ee1e9f8f503711457a17e90da7
cengle <go2engle@gmail.com>
2022-03-02 19:03:32 +0000

    modify install command
    added --accept-package-agreements --accept-source-agreements

--- Install.ps1
+++ Install.ps1
@@ -1,16 +1,16 @@
-winget install -e --id Prusa3D.PrusaSlicer
-winget install -e --id Ultimaker.Cura
-winget install -e --id Mozilla.Firefox
-winget install -e --id Notepad++.Notepad++
-winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Valve.Steamwinget install -e --id suse.RancherDesktop
-winget install -e --id mRemoteNG.mRemoteNG
-winget install -e --id Greenshot.Greenshot
-winget install -e --id Joplin.Joplin
-winget install -e --id GitHub.GitHubDesktop
-winget install -e --id Discord.Discord
-winget install -e --id RocketChat.RocketChat
-winget install -e --id Mirantis.Lens
-winget install -e --id 7zip.7zip
-winget install -e --id EpicGames.EpicGamesLauncher
-winget install -e --id Git.Git
+winget install -e --accept-package-agreements --accept-source-agreements --id Prusa3D.PrusaSlicer
+winget install -e --accept-package-agreements --accept-source-agreements --id Ultimaker.Cura
+winget install -e --accept-package-agreements --accept-source-agreements --id Mozilla.Firefox
+winget install -e --accept-package-agreements --accept-source-agreements --id Notepad++.Notepad++
+winget install -e --accept-package-agreements --accept-source-agreements --id Microsoft.VisualStudioCode
+winget install -e --accept-package-agreements --accept-source-agreements --id Valve.Steamwinget install -e --id suse.RancherDesktop
+winget install -e --accept-package-agreements --accept-source-agreements --id mRemoteNG.mRemoteNG
+winget install -e --accept-package-agreements --accept-source-agreements --id Greenshot.Greenshot
+winget install -e --accept-package-agreements --accept-source-agreements --id Joplin.Joplin
+winget install -e --accept-package-agreements --accept-source-agreements --id GitHub.GitHubDesktop
+winget install -e --accept-package-agreements --accept-source-agreements --id Discord.Discord
+winget install -e --accept-package-agreements --accept-source-agreements --id RocketChat.RocketChat
+winget install -e --accept-package-agreements --accept-source-agreements --id Mirantis.Lens
+winget install -e --accept-package-agreements --accept-source-agreements --id 7zip.7zip
+winget install -e --accept-package-agreements --accept-source-agreements --id EpicGames.EpicGamesLauncher
+winget install -e --accept-package-agreements --accept-source-agreements --id Git.Git
