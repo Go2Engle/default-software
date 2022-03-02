b409c3d283c931e7a36b11c7d8f520290925dfcb
Chris Engle <go2engle@gmail.com>
2022-03-02 10:54:38 -0500

    changed formating

--- Install.ps1
+++ Install.ps1
@@ -1,1 +1,14 @@
-winget install -e --id Prusa3D.PrusaSlicer;winget install -e --id Ultimaker.Cura;winget install -e --id Mozilla.Firefox;winget install -e --id Notepad++.Notepad++;winget install -e --id Microsoft.VisualStudioCode;winget install -e --id Valve.Steam;winget install -e --id suse.RancherDesktop;winget install -e --id mRemoteNG.mRemoteNG;winget install -e --id Greenshot.Greenshot;winget install -e --id Joplin.Joplin;winget install -e --id GitHub.GitHubDesktop;winget install -e --id Discord.Discord;winget install -e --id RocketChat.RocketChat;winget install -e --id Mirantis.Lens;winget install -e --id 7zip.7zip
+winget install -e --id Prusa3D.PrusaSlicer
+winget install -e --id Ultimaker.Cura
+winget install -e --id Mozilla.Firefox
+winget install -e --id Notepad++.Notepad++
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Valve.Steamwinget install -e --id suse.RancherDesktop
+winget install -e --id mRemoteNG.mRemoteNG
+winget install -e --id Greenshot.Greenshot
+winget install -e --id Joplin.Joplin
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Discord.Discord
+winget install -e --id RocketChat.RocketChat
+winget install -e --id Mirantis.Lens
+winget install -e --id 7zip.7zip
